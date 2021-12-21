6dedae360cb24e49cd7aa0b107cd00392b012903
David W. Roscoe <DWRoscoe@Comcast.net>
2021-12-21 17:23:40 -0500

    Initial set of project files including class test.java.

---
+++ versionTest/src/versionTest/test.java
@@ -0,0 +1,14 @@
+package versionTest;
+
+public class test {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+  System.out.println("main(..) test 1.");
+  System.out.println("main(..) test 2.");
+  System.out.println("main(..) test 3.");
+  System.out.println("main(..) test 4.");
+  System.out.println("main(..) test 5.");
+  }
+
+}
